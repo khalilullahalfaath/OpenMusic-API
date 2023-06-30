3a8ac310c03a2ea4e9cc7a6a689ed56f8b907411
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-06-30 12:24:19 +0700

    implements routing

---
+++ src/api/albums/routes.js
@@ -0,0 +1,24 @@
+const routes = () => [
+  {
+    method: 'POST',
+    path: '/albums',
+    handler: () => {},
+  },
+  {
+    method: 'GET',
+    path: '/albums/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'PUT',
+    path: '/albums/{id}',
+    handler: () => {},
+  },
+  {
+    method: 'POST',
+    path: '/albums/{id}',
+    handler: () => {},
+  },
+];
+
+module.exports = routes;
