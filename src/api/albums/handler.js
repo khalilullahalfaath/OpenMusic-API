34a8b8eb02fe11c4b295b68071899a540f099d8d
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-07-19 10:35:54 +0700

    fix redundant code in albums handler

--- src/api/albums/handler.js
+++ src/api/albums/handler.js
@@ -10,9 +10,8 @@ class AlbumsHandler {
 
   async postAlbumHandler(request, h) {
     this._validator.validateAlbumPayload(request.payload);
-    const { name, year } = request.payload;
 
-    const albumId = await this._service.addAlbum({ name, year });
+    const albumId = await this._service.addAlbum(request.payload);
 
     const response = h.response({
       status: 'success',
@@ -38,10 +37,10 @@ class AlbumsHandler {
 
   async putAlbumByIdHandler(request) {
     this._validator.validateAlbumPayload(request.payload);
-    const { name, year } = request.payload;
+
     const { id } = request.params;
 
-    await this._service.editAlbumById(id, { name, year });
+    await this._service.editAlbumById(id, request.payload);
 
     return {
       status: 'success',
