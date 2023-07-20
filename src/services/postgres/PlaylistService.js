baf27e49f93770aca98a505ccf2e7db4a9f7a44a
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-07-20 14:18:53 +0700

    finish implementation of playlist services

--- src/services/postgres/PlaylistService.js
+++ src/services/postgres/PlaylistService.js
@@ -67,6 +67,63 @@ class PlaylistsService {
       throw new NotFoundError('Playlist gagal dihapus. Id tidak ditemukan');
     }
   }
+
+  async addSongToPlaylist(playlistId, songId) {
+    const id = `playlist_song-${nanoid(16)}`;
+
+    const query = {
+      text: 'INSERT INTO playlist_songs VALUES($1, $2, $3) RETURNING id',
+      values: [id, playlistId, songId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Lagu gagal ditambahkan ke playlist');
+    }
+
+    return result.rows[0].id;
+  }
+
+  async getSongsFromPlaylist(playlistId) {
+    const playlistWithUserQuery = {
+      text: 'SELECT playlist.* , users.username FROM playlists LEFT JOIN users ON users.id = playlists.owner WHERE playlists.id = $1',
+      values: [playlistId],
+    };
+
+    const resultPlaylistWithUser = await this._pool.query(playlistWithUserQuery);
+
+    if (!resultPlaylistWithUser.rows.length) {
+      throw new NotFoundError('Playlist tidak ditemukan');
+    }
+
+    const songsQuery = {
+      text: 'SELECT songs.* FROM songs LEFT JOIN playlist_songs ON playlist_songs.song_id = songs.id WHERE playlist_songs.playlist_id = $1',
+      values: [playlistId],
+    };
+
+    const resultSongsQuery = await this._pool.query(songsQuery);
+
+    return {
+      ...resultPlaylistWithUser.rows[0],
+      songs: resultSongsQuery.rows,
+    };
+  }
+
+  async deleteSongFromPlaylistById(playlistId, songId) {
+    const query = {
+      text: 'DELETE FROM playlist_songs WHERE playlist_id = $1 AND song_id = $2 RETURNING id',
+      values: [playlistId, songId],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Lagu gagal dihapus dari playlist. Id tidak ditemukan');
+    }
+
+    return result.rows[0].id;
+  }
 }
 
 module.exports = PlaylistsService;
