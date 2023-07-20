2a34ae792b6e73eb5ba75b1ad3d2d7ccd40159b4
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-07-20 13:50:55 +0700

    implement function post, get, and delete for table playlist

--- migrations/1689759230702_create-table-playlists.js
+++ migrations/1689759230702_create-table-playlists.js
@@ -12,6 +12,14 @@ exports.up = (pgm) => {
       type: 'VARCHAR(50)',
       notNull: true,
     },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
   });
 };
 
--- src/services/postgres/PlaylistService.js
+++ src/services/postgres/PlaylistService.js
@@ -1,5 +1,8 @@
 const { Pool } = require('pg');
 const { nanoid } = require('nanoid');
+const InvariantError = require('../../exceptions/InvariantError');
+const NotFoundError = require('../../exceptions/NotFoundError');
+const AuthorizationError = require('../../exceptions/AuthorizationError');
 
 class PlaylistsService {
   constructor() {
@@ -22,4 +25,48 @@ class PlaylistsService {
 
     return result.rows[0].id;
   }
+
+  async getPlaylists(owner) {
+    const query = {
+      text: 'SELECT * FROM playlists WHERE owner = $1',
+      values: [owner],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows;
+  }
+
+  async verifyPlaylistOwner(id, owner) {
+    const query = {
+      text: 'SELECT * FROM playlists WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Playlist tidak ditemukan');
+    }
+
+    const playlist = result.rows[0];
+
+    if (playlist.owner !== owner) {
+      throw new AuthorizationError('Anda tidak berhak mengakses resource ini');
+    }
+  }
+
+  async deletePlaylistById(id) {
+    const query = {
+      text: 'DELETE FROM playlists WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Playlist gagal dihapus. Id tidak ditemukan');
+    }
+  }
 }
+
+module.exports = PlaylistsService;
