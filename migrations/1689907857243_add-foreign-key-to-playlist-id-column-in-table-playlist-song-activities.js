181fd0da2b790b45ce88c3fe6a84d7b598ae3e3e
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-07-21 10:24:30 +0700

    implement migration for playlist song activities

---
+++ migrations/1689907857243_add-foreign-key-to-playlist-id-column-in-table-playlist-song-activities.js
@@ -0,0 +1,15 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.addConstraint(
+    'playlist_song_activities',
+    'fk_playlist_song_activities.playlist_id_playlists.id',
+    'FOREIGN KEY(playlist_id) REFERENCES playlists(id) ON DELETE CASCADE',
+  );
+};
+
+exports.down = (pgm) => {
+  pgm.dropConstraint('playlist_song_activities', 'fk_playlist_song_activities.playlist_id_playlists.id');
+};
