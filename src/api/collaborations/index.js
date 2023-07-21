f6450d0511e38b572fcac6db5cc0adbdc69d3694
@khalilullah.alfaath21 <khalilullah.alfaath21@gmail.com>
2023-07-21 09:29:16 +0700

    implement collaboration plugin

---
+++ src/api/collaborations/index.js
@@ -0,0 +1,19 @@
+const CollaborationHandler = require('./handler');
+const routes = require('./routes');
+
+module.exports = {
+  name: 'collaborations',
+  version: '1.0.0',
+  register: async (server, {
+    collaborationsService,
+    playlistsService,
+    validator,
+  }) => {
+    const collaborationHandler = new CollaborationHandler(
+      collaborationsService,
+      playlistsService,
+      validator,
+    );
+    server.route(routes(collaborationHandler));
+  },
+};
